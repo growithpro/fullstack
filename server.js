979b981174e258c32d3a00b0339c438d8b780ce3
Naveen Joshi <naveen@GROWITHPRO.>
2026-04-20 22:01:45 +0530

    First Backend App Initialised

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from Backend " });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is Running at ${PORT}`)
+})

e24a47ac8f2fcca6bcfb75b388eae0d6e389a5f4
Naveen Joshi <naveen@GROWITHPRO.>
2026-04-21 22:12:32 +0530

    Create backend.yml and change in server.js

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from Backend, its new message " });
 })
 
+app.get("/api/products", (req, res) => {
+    res.json({ message: "All Products" })
+})
+
 app.listen(PORT, () => {
     console.log(`Server is Running at ${PORT}`)
 })
